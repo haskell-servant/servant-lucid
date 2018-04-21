7f43f18ec21b51ab1f328f70d61c62cb13b7902a
David Baynard <davidbaynard@gmail.com>
2018-04-21 18:45:52 +0100

    Update test

--- example/Main.hs
+++ example/Main.hs
@@ -4,6 +4,7 @@
 module Main (main) where
 
 import Lucid
+import Lucid.Servant
 import Data.Maybe         (fromMaybe)
 import Network.Wai        (Application)
 import Servant
@@ -14,17 +15,21 @@ import Text.Read          (readMaybe)
 import qualified Network.Wai.Handler.Warp as Warp
 
 type API = "string" :> Get '[HTML] String
-    :<|> "html" :> Get '[HTML] (Html ())
+    :<|> "nested" :> "html" :> Get '[HTML] (Html ())
 
 api :: Proxy API
 api = Proxy
+
+stringLink :: Link
+stringLink = safeLink (Proxy :: Proxy API) (Proxy :: Proxy ("string" :> Get '[HTML] String))
 
 server :: Server API
 server = return "example" :<|> return html where
     html :: Html ()
     html = do
-        b_ "bar"
-        i_ "iik"
+        p_ $ b_ "bar"
+        p_ $ i_ "iik"
+        p_ $ a_ [safeHref_ stringLink] "string"
 
 app :: Application
 app = serve api server
